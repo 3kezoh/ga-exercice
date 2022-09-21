b4384566eb1d38142d7105403c7e0166e659e53a
3kezoh <3kezoh@gmail.com>
2022-09-21 09:10:02 +0200

    👔 Add margherita

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-const pizza = {
-  name: "Diavola",
-  price: 8,
-};
+const pizzas = [
+  {
+    name: "Diavola",
+    price: 8,
+  },
+  {
+    name: "Margherita",
+    price: 6,
+  },
+];
 
-console.log(pizza);
+console.log(pizzas);
